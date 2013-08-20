c495218daed561a147c0a4a0eca838a3fc6a21e2
Riccardo Solmi <solmi.riccardo@gmail.com>
2013-08-20 09:28:00 +0200

    Add Sort helper

--- org.whole.langs.core/src/org/whole/lang/queries/util/QueriesUtils.java
+++ org.whole.langs.core/src/org/whole/lang/queries/util/QueriesUtils.java
@@ -17,13 +17,22 @@
  */
 package org.whole.lang.queries.util;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
 import org.whole.lang.commons.parsers.CommonsDataTypePersistenceParser;
 import org.whole.lang.commons.reflect.CommonsEntityDescriptorEnum;
+import org.whole.lang.comparators.BusinessIdentityComparator;
 import org.whole.lang.iterators.IEntityIterator;
 import org.whole.lang.iterators.IteratorFactory;
 import org.whole.lang.model.IEntity;
 import org.whole.lang.queries.factories.QueriesEntityFactory;
 import org.whole.lang.queries.model.Path;
+import org.whole.lang.reflect.EntityDescriptor;
+import org.whole.lang.reflect.FeatureDescriptor;
+import org.whole.lang.util.DataTypeUtils;
 import org.whole.lang.util.EntityUtils;
 
 /**
@@ -48,5 +57,54 @@ public class QueriesUtils {
 			parent = child;
 		}
 		return path;
+	}
+
+	public static IEntity sort(IEntity entity) {
+		if (entity.wGetEntityDescriptor().getCompositeKind().isOrdered()) {
+			List<IEntity> entityList = (List<IEntity>) entity;
+			IEntity[] array = entityList.toArray(new IEntity[entity.wSize()]);
+			entityList.clear();
+			Arrays.sort(array, new Comparator<IEntity>() {
+				public int compare(IEntity e1, IEntity e2) {
+					IEntity e1Adaptee = e1.wGetAdaptee(false);
+					IEntity e2Adaptee = e2.wGetAdaptee(false);
+					EntityDescriptor<?> e1Ed = e1Adaptee.wGetEntityDescriptor();
+					EntityDescriptor<?> e2Ed = e2Adaptee.wGetEntityDescriptor();
+					if (e1Ed != e2Ed)
+						return 0;
+					switch (e1Ed.getEntityKind()) {
+					case DATA:
+						return DataTypeUtils.compare(e1Adaptee, e2Adaptee);
+					default:
+					case COMPOSITE:
+						return 0;
+					case SIMPLE:
+						boolean idBased = false;
+						List<FeatureDescriptor> entityFeatureDescriptors = e1Ed.getEntityFeatureDescriptors();
+						for (FeatureDescriptor fd : entityFeatureDescriptors) {
+							if (fd.isId()) {
+								idBased = true;
+								int result = compare(e1Adaptee.wGet(fd), e2Adaptee.wGet(fd));
+								if (result != 0)
+									return result;
+							}
+						}
+						if (idBased)
+							return 0;
+						for (FeatureDescriptor fd : entityFeatureDescriptors) {
+							int result = compare(e1Adaptee.wGet(fd), e2Adaptee.wGet(fd));
+							if (result != 0)
+								return result;
+						}
+						return 0;
+					}
+				}
+			});
+			for (IEntity child : array)
+				entity.wAdd(child);
+			return entity;
+		} else
+			return entity;
+		
 	}
 }
