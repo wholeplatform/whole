e2e0a228149ec40e459cf5c69ad1ca514332d575
Enrico Persiani <enrico.persiani@metatis.com>
2013-12-03 15:22:32 +0100

    Add missing java docs and perform some cleanup

--- org.whole.releng/maven-testapp/src/main/java/org/whole/test/WholeTestApp.java
+++ org.whole.releng/maven-testapp/src/main/java/org/whole/test/WholeTestApp.java
@@ -1,11 +1,27 @@
 package org.whole.test;
 
+/**
+ * Copyright 2004-2013 Riccardo Solmi. All rights reserved.
+ * This file is part of the Whole Platform.
+ *
+ * The Whole Platform is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * The Whole Platform is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with the Whole Platform. If not, see <http://www.gnu.org/licenses/>.
+ */
 import org.whole.lang.reflect.ReflectionFactory;
 import org.whole.lang.bindings.BindingManagerFactory;
 import org.whole.lang.bindings.IBindingManager;
 import org.whole.lang.util.BehaviorUtils;
 import org.whole.lang.model.IEntity;
-import org.whole.lang.queries.util.QueriesUtils;
 import org.whole.lang.queries.model.PathExpression;
 import org.whole.lang.operations.PrettyPrinterOperation;
 import org.whole.lang.models.codebase.ArtifactsModel;
@@ -13,8 +29,10 @@ import org.whole.lang.models.model.Model;
 import org.whole.lang.xsd.codebase.ModelToXmlSchemaQuery;
 import org.whole.lang.iterators.IEntityIterator;
 import org.whole.lang.iterators.IteratorFactory;
-import org.whole.lang.models.model.Model;
 
+/**
+ * @author Enrico Persiani
+ */
 public class WholeTestApp {
 	public static void main( String[] args ) {
 		// deploy the platform
@@ -27,8 +45,8 @@ public class WholeTestApp {
 		IBindingManager bm = BindingManagerFactory.instance.createArguments();
 		IEntity artifactsSchema = BehaviorUtils.evaluateFirstResult(path, artifactsModel, bm);
 		PrettyPrinterOperation.prettyPrint(artifactsSchema);
-		
-		IEntityIterator iterator = IteratorFactory.childIterator();
+
+		IEntityIterator<?> iterator = IteratorFactory.childIterator();
 		iterator.reset(artifactsModel.getDeclarations());
 		while (iterator.hasNext()) {
 			IEntity entity = (IEntity) iterator.next();
--- org.whole.releng/maven-testapp/src/test/java/org/whole/test/WholeTestAppTest.java
+++ org.whole.releng/maven-testapp/src/test/java/org/whole/test/WholeTestAppTest.java
@@ -1,23 +1,43 @@
+/**
+ * Copyright 2004-2013 Riccardo Solmi. All rights reserved.
+ * This file is part of the Whole Platform.
+ *
+ * The Whole Platform is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * The Whole Platform is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with the Whole Platform. If not, see <http://www.gnu.org/licenses/>.
+ */
 package org.whole.test;
 
 import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
 
+/**
+ * @author Enrico Persiani
+ */
 public class WholeTestAppTest extends TestCase {
-    public WholeTestAppTest( String testName ) {
-        super( testName );
-    }
-
-    public static Test suite() {
-        return new TestSuite( WholeTestAppTest.class );
-    }
-
-    public void testApp() {
-	try {
-            WholeTestApp.main(new String[0]);
-	} catch (Exception e) {
-            fail();
+	public WholeTestAppTest( String testName ) {
+		super( testName );
 	}
-    }
+
+	public static Test suite() {
+		return new TestSuite( WholeTestAppTest.class );
+	}
+
+	public void testApp() {
+		try {
+			WholeTestApp.main(new String[0]);
+		} catch (Exception e) {
+			fail();
+		}
+	}
 }
