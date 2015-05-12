86092c9a4b5a3d92c8ebf34b895530081fa566fb
Riccardo Solmi <solmi.riccardo@gmail.com>
2015-05-12 18:22:29 +0200

    Update Copyright year [skip ci]

--- org.whole.lang.e4.ui/src/org/whole/lang/e4/ui/menu/EntityAssistCompositeContributionItem.java
+++ org.whole.lang.e4.ui/src/org/whole/lang/e4/ui/menu/EntityAssistCompositeContributionItem.java
@@ -1,5 +1,5 @@
 /**
- * Copyright 2004-2014 Riccardo Solmi. All rights reserved.
+ * Copyright 2004-2015 Riccardo Solmi. All rights reserved.
  * This file is part of the Whole Platform.
  *
  * The Whole Platform is free software: you can redistribute it and/or modify
