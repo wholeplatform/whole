28b52228451208d921a8c105b6d69ef5f126fc66
Enrico Persiani <enrico.persiani@metatis.com>
2016-02-25 10:53:31 +0100

    Fix unknown fingerprint in ldk release

--- ci-release.sh
+++ ci-release.sh
@@ -1,6 +1,7 @@
 # To test the ci release locally use the following command:
 # sh -x ci-release.sh
 
+ssh-keyscan -H web.sourceforge.net >> ~/.ssh/known_hosts
 CI_BUILD_DIR=$(pwd)
 cd .ci-build/whole/org.whole.products.releng
 gradle --quiet release_lw release_ldk
