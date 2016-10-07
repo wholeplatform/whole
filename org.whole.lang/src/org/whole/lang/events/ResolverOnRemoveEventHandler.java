d87acd9fc8d08defe46449f6e44517c81ca23265
Riccardo Solmi <solmi.riccardo@gmail.com>
2016-10-07 19:30:14 +0200

    Add generic on remove event handlers
    [skip ci]

---
+++ org.whole.lang/src/org/whole/lang/events/ResolverOnRemoveEventHandler.java
@@ -0,0 +1,41 @@
+/**
+ * Copyright 2004-2016 Riccardo Solmi. All rights reserved.
+ * This file is part of the Whole Platform.
+ *
+ * The Whole Platform is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * The Whole Platform is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with the Whole Platform. If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.whole.lang.events;
+
+import org.whole.lang.commons.factories.CommonsEntityFactory;
+import org.whole.lang.model.IEntity;
+import org.whole.lang.reflect.FeatureDescriptor;
+
+/**
+ * @author Riccardo Solmi
+ */
+public class ResolverOnRemoveEventHandler extends IdentityEventHandler {
+	private static final long serialVersionUID = 1L;
+	private IRemovedEventActionEnabler actionEnabler;
+
+	public ResolverOnRemoveEventHandler(IRemovedEventActionEnabler enabler) {
+		this.actionEnabler = enabler;
+	}
+
+	@Override
+	public void notifyRemoved(IEntity source, FeatureDescriptor fd, int index, IEntity oldValue) {
+		if (!source.wGetModel().getCompoundModel().getHistoryManager().isHistoryEvent() &&
+				actionEnabler.apply(source, fd, index, oldValue))
+			source.wAdd(index, CommonsEntityFactory.instance.createResolver());
+	}
+}
