f6856063446afbdffa9790ad7c2fba2977272d71
Riccardo Solmi <solmi.riccardo@gmail.com>
2018-08-18 16:05:36 +0200

    Add tester for steppers
    [skip ci]

--- org.whole.test/test/org/whole/lang/executables/ExecutableFactoryTest.java
+++ org.whole.test/test/org/whole/lang/executables/ExecutableFactoryTest.java
@@ -20,6 +20,9 @@ package org.whole.lang.executables;
 import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertSame;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.junit.BeforeClass;
 import org.junit.Test;
 import org.whole.lang.bindings.BindingManagerFactory;
@@ -27,6 +30,7 @@ import org.whole.lang.iterators.IEntityIterator;
 import org.whole.lang.iterators.IteratorFactory;
 import org.whole.lang.model.IEntity;
 import org.whole.lang.reflect.ReflectionFactory;
+import org.whole.lang.steppers.IDataFlowConsumer;
 
 /**
  * @author Riccardo Solmi
@@ -51,8 +55,165 @@ public class ExecutableFactoryTest {
     	};
     }
 
+	public static enum Event {
+		BEGIN, NEXT, END
+	}
+
+    public static class TesterDataFlowConsumer implements IDataFlowConsumer {
+    	public List<Event> events = new ArrayList<>();
+    	public List<IEntity> values = new ArrayList<>();
+    	protected Event[] expectedEvents;
+    	protected IEntity[] expectedValues;
+
+    	public void clear() {
+    		events.clear();
+    		values.clear();
+    	}
+
+    	public void setExpectedEvents(Event... events) {
+			this.expectedEvents = events;
+		}
+
+    	public void setExpectedValues(IEntity... values) {
+			this.expectedValues = values;
+		}
+
+    	protected void addValue(IEntity e) {
+    		values.add(e);
+
+    		if (expectedValues != null) {
+    			if (expectedValues.length < values.size())
+    				throw new IllegalStateException("Received extra: "+e);
+    			if (expectedValues[values.size()-1] != e)
+    				throw new IllegalStateException(values.size()+"th value expected: "+expectedValues[values.size()-1]+"\n\nReceived: "+e);
+    		}
+    	}
+
+    	protected void addEvent(Event e) {
+    		events.add(e);
+
+    		if (expectedEvents != null) {
+    			if (expectedEvents.length < events.size())
+    				throw new IllegalStateException("Received extra: "+e);
+    			if (expectedEvents[events.size()-1] != e)
+    				throw new IllegalStateException(events.size()+"th event expected: "+expectedEvents[events.size()-1]+" received: "+e);
+    		}
+    	}
+
+		public void doBegin(int size) {
+			addEvent(Event.BEGIN);
+		}
+
+		public void doNext(IEntity entity) {
+			addEvent(Event.NEXT);
+			addValue(entity);
+		}
+
+		public void doEnd() {
+			addEvent(Event.END);
+		}
+    }
+
     @Test
-    public void testEmpty() {
+    public void testEmptyStepper() {
+    	IEntityIterator<?> p = f.emptyIterator();
+    	p.setBindings(bmf.createBindingManager());
+    	p.reset(values[0]);
+
+    	
+    	TesterDataFlowConsumer c = new TesterDataFlowConsumer();
+		p.withConsumer(c);
+
+    	c.setExpectedEvents(Event.END);
+    	p.callNext();
+
+    	c.clear();
+    	c.setExpectedEvents(Event.END, Event.END);
+    	p.callNext();
+    	p.callNext();
+    }
+
+    @Test
+    public void testConstantStepper() {
+    	IEntityIterator<?> p = f.constantIterator(values[0], false);
+    	p.setBindings(bmf.createBindingManager());
+    	p.reset(values[1]);
+
+    	TesterDataFlowConsumer c = new TesterDataFlowConsumer();
+		p.withConsumer(c);
+
+    	c.setExpectedValues(values[0]);
+    	c.setExpectedEvents(Event.NEXT, Event.END);
+    	p.reset(values[1]);
+    	p.callNext();
+    	p.callRemaining();
+    	 
+    	c.clear();
+     	p.reset(values[1]);
+    	p.callRemaining();
+
+    	c.clear();
+    	p.reset(values[1]);
+    	p.callNext();
+    	p.callNext();
+
+    	c.setExpectedEvents(Event.NEXT, Event.END, Event.END, Event.END);
+    	c.clear();
+    	p.reset(values[1]);
+    	p.callNext();
+    	p.callNext();
+    	p.callNext();
+    	p.callNext();
+ 
+    	c.clear();
+    	p.reset(values[1]);
+    	p.callRemaining();
+    	p.callRemaining();
+    	p.callRemaining();
+    }
+
+    @Test
+    public void testSequenceStepper() {
+    	@SuppressWarnings("unchecked")
+		IEntityIterator<?> p = f.sequenceIterator(
+    			f.constantIterator(values[0], false),
+    			f.constantIterator(values[1], false),
+    			f.constantIterator(values[2], false),
+    			f.sequenceIterator(
+    	    			f.constantIterator(values[3], false),
+    	    			f.constantIterator(values[4], false)),
+    			f.sequenceIterator(
+    	    			f.constantIterator(values[5], false),
+    	    			f.constantIterator(values[6], false),
+    	    			f.constantIterator(values[7], false))
+    	);
+    	p.setBindings(bmf.createBindingManager());
+
+    	TesterDataFlowConsumer c = new TesterDataFlowConsumer();
+		p.withConsumer(c);
+		c.setExpectedValues(
+    			values[0], values[1], values[2], values[3], values[4], values[5], values[6], values[7]);
+    	c.setExpectedEvents(
+    			Event.NEXT, Event.NEXT, Event.NEXT, Event.NEXT, Event.NEXT, Event.NEXT, Event.NEXT, Event.NEXT, Event.END);
+
+    	p.reset(values[0]);
+    	p.callNext();
+    	p.callNext();
+    	p.callNext();
+    	p.callNext();
+    	p.callNext();
+    	p.callNext();
+    	p.callNext();
+    	p.callNext();
+    	p.callNext();
+ 
+    	c.clear();
+    	p.reset(values[0]);
+    	p.callRemaining();
+    }
+
+    @Test
+    public void testEmptyEvaluator() {
     	IEntityIterator<?> i = f.emptyIterator();
     	i.setBindings(bmf.createBindingManager());
     	i.reset(values[0]);
@@ -71,7 +232,7 @@ public class ExecutableFactoryTest {
     }
 
     @Test
-    public void testConstant() {
+    public void testConstantEvaluator() {
     	IEntityIterator<?> i = f.constantIterator(values[0], false);
     	i.setBindings(bmf.createBindingManager());
     	i.reset(values[1]);
@@ -93,7 +254,7 @@ public class ExecutableFactoryTest {
     }
 
     @Test
-    public void testSequence() {
+    public void testSequenceEvaluator() {
     	@SuppressWarnings("unchecked")
 		IEntityIterator<?> i = f.sequenceIterator(
     			f.constantIterator(values[0], false),
@@ -129,7 +290,7 @@ public class ExecutableFactoryTest {
     }
 
     @Test
-    public void testChooseByOrder() {
+    public void testChooseByOrderEvaluator() {
     	@SuppressWarnings("unchecked")
 		IEntityIterator<?> i = f.chooseIterator(
     			f.emptyIterator(),
