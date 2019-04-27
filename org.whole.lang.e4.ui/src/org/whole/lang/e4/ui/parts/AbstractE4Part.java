c9e4eee1ec228022a0c9e384a31348384793e015
Enrico Persiani <enrico.persiani@metatis.com>
2019-04-27 18:56:03 +0200

    Minor improvements
    [skip ci]

--- org.whole.lang.e4.ui/src/org/whole/lang/e4/ui/parts/AbstractE4Part.java
+++ org.whole.lang.e4.ui/src/org/whole/lang/e4/ui/parts/AbstractE4Part.java
@@ -60,8 +60,6 @@ import org.eclipse.swt.events.FocusAdapter;
 import org.eclipse.swt.events.FocusEvent;
 import org.eclipse.swt.layout.FillLayout;
 import org.eclipse.swt.widgets.Composite;
-import org.eclipse.swt.widgets.Control;
-import org.eclipse.swt.widgets.Display;
 import org.whole.lang.bindings.IBindingManager;
 import org.whole.lang.codebase.IFilePersistenceProvider;
 import org.whole.lang.codebase.IPersistenceProvider;
