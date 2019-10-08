e33d02536e37fe315aad59e40ef23c1861d228c8
Riccardo Solmi <solmi.riccardo@gmail.com>
2019-10-08 12:57:59 +0200

    FIX require bundle for Eclipse 4.13

---
+++ org.whole.lang/src/org/whole/lang/evaluators/ReferenceEvaluator.java
@@ -0,0 +1,40 @@
+/**
+ * Copyright 2004-2019 Riccardo Solmi. All rights reserved.
+ * This file is part of the Whole Platform.
+ *
+ * The Whole Platform is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * The Whole Platform is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with the Whole Platform. If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.whole.lang.evaluators;
+
+import org.whole.lang.executables.IExecutable;
+import org.whole.lang.model.IEntity;
+import org.whole.lang.operations.ICloneContext;
+
+/**
+ * @author Riccardo Solmi
+ */
+public class ReferenceEvaluator extends ConstantEvaluator {
+	public ReferenceEvaluator(IEntity reference) {
+		super(reference, false);
+	}
+
+	@Override
+	public IExecutable clone(ICloneContext cc) {
+		ReferenceEvaluator executor = (ReferenceEvaluator) super.clone(cc);
+		executor.constant.wSetValue(cc.differentiate((IExecutable) constant.wGetValue()));
+		if (executor.nextConstant != null)
+			executor.nextConstant.wSetValue(cc.differentiate((IExecutable) nextConstant.wGetValue()));
+		return executor;
+	}
+}
